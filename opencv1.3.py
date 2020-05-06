9574609c9df2cd05c07f7d00c4e2c0523c5aa403
KRISHANU SAINI <56930593+krishanu-2001@users.noreply.github.com>
2020-05-06 14:33:48 +0530

    file impro

--- opencv1.3.py
+++ opencv1.3.py
@@ -1,11 +1,12 @@
 # import the necessary packages
-from collections import deque
+
 from imutils.video import VideoStream
-import numpy as np
-import argparse
-import cv2
 import imutils
 import time
+import numpy as np
+import argparse
+from collections import deque
+import cv2
 import pyautogui
 import threading
 pyautogui.PAUSE = 0.05
@@ -21,100 +22,52 @@ def posinside(x_,y_):
     else:
         return False
 
-
-# construct the argument parse and parse the arguments
-ap = argparse.ArgumentParser()
-ap.add_argument("-v", "--video",
+rain = argparse.ArgumentParser()
+rain.add_argument("-v", "--video",
 	help="path to the (optional) video file")
-ap.add_argument("-b", "--buffer", type=int, default=64,
+rain.add_argument("-b", "--buffer", type=int, default=64,
 	help="max buffer size")
-args = vars(ap.parse_args())
-# define the lower and upper boundaries of the "green"
-# ball in the HSV color space, then initialize the
-# list of tracked points
-greenLower = (29, 86, 6)
-greenUpper = (64, 255, 255)
+args = vars(rain.parse_args())
+color_r1 = (30, 100, 20)
+color_r2 = (60, 255, 240)
 pts = deque(maxlen=args["buffer"])
-# if a video path was not supplied, grab the reference
-# to the webcam
 if not args.get("video", False):
-	vs = VideoStream(src=0).start()
-# otherwise, grab a reference to the video file
+	cv = VideoStream(src=0).start()
 else:
-	vs = cv2.VideoCapture(args["video"])
-# allow the camera or video file to warm up
-time.sleep(2.0)
-# keep looping
+	cv = cv2.VideoCapture(args["video"])
+time.sleep(1.0)
 def f1():
     while True:
-        # grab the current frame
-        frame = vs.read()
-        # handle the frame from VideoCapture or VideoStream
-        frame = frame[1] if args.get("video", False) else frame
-        # if we are viewing a video and we did not grab a frame,
-        # then we have reached the end of the video
+        frame = cv.read()
+        frame = frame
+        if args.get("video", False):
+            frame = frame[1]
+        else:
+            frame = frame
         if frame is None:
             break
-        # resize the frame, blur it, and convert it to the HSV
-        # color space
-        frame = imutils.resize(frame, width=600)
-        blurred = cv2.GaussianBlur(frame, (11, 11), 0)
-        hsv = cv2.cvtColor(blurred, cv2.COLOR_BGR2HSV)
-        # construct a mask for the color "green", then perform
-        # a series of dilations and erosions to remove any small
-        # blobs left in the mask
-        mask = cv2.inRange(hsv, greenLower, greenUpper)
-        mask = cv2.erode(mask, None, iterations=2)
-        mask = cv2.dilate(mask, None, iterations=2)
-        # find contours in the mask and initialize the current
-        # (x, y) center of the ball
-        cnts = cv2.findContours(mask.copy(), cv2.RETR_EXTERNAL,
-                            cv2.CHAIN_APPROX_SIMPLE)
-        cnts = imutils.grab_contours(cnts)
-        center = None
-        # only proceed if at least one contour was found
-        if len(cnts) > 0:
-            # find the largest contour in the mask, then use
-            # it to compute the minimum enclosing circle and
-            # centroid
-            c = max(cnts, key=cv2.contourArea)
-            ((x, y), radius) = cv2.minEnclosingCircle(c)
-            M = cv2.moments(c)
-            center = (int(M["m10"] / M["m00"]), int(M["m01"] / M["m00"]))
-            # only proceed if the radius meets a minimum size
-            if radius > 10:
-                # draw the circle and centroid on the frame,
-                # then update the list of tracked points
-                cv2.circle(frame, (int(x), int(y)), int(radius),
-                           (0, 255, 255), 2)
-                cv2.circle(frame, center, 5, (0, 0, 255), -1)
+        frame = imutils.resize(frame, width=500)
+        isblr = cv2.GaussianBlur(frame, (11, 11), 0)
+        sauce = cv2.cvtColor(isblr, cv2.COLOR_BGR2HSV)
+        #first things required for extracting information.
+        mask = cv2.inRange(sauce, color_r1, color_r2)
+        mask = cv2.erode(mask, None, iterations=1)
+        mask = cv2.dilate(mask, None, iterations=1)
+        cnter = cv2.findContours(mask.copy(), cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+        cnter = imutils.grab_contours(cnter)
+        centroid = None
+        if f3(cnter) >0:
+            card = max(cnter, key=cv2.contourArea)
+            ((x, y), radius) = cv2.minEnclosingCircle(card)
+            joe = cv2.moments(card)
+            centroid = (int(joe["m10"] / joe["m00"]), int(joe["m01"] / joe["m00"]))
 
-        # update the points queue
-        pts.appendleft(center)
-        # loop over the set of tracked points
-        for i in range(1, len(pts)):
-            # if either of the tracked points are None, ignore
-            # them
-            if pts[i - 1] is None or pts[i] is None:
-                continue
-            # otherwise, compute the thickness of the line and
-            # draw the connecting lines
-            thickness = int(np.sqrt(args["buffer"] / float(i + 1)) * 2.5)
-            cv2.line(frame, pts[i - 1], pts[i], (0, 0, 255), thickness)
-            #mousemove(pts[i-1], pts[i])
-        # show the frame to our screen
-        cv2.imshow("Frame", frame)
-        key = cv2.waitKey(1) & 0xFF
-        # if the 'q' key is pressed, stop the loop
-        if key == ord("q"):
-            break
-    # if we are not using a video file, stop the camera video stream
+        if centroid != np.NaN:
+            pts.appendleft(centroid)
     if not args.get("video", False):
-        vs.stop()
-    # otherwise, release the camera
+        cv.stop()
     else:
-        vs.release()
-    # close all windows
+        cv.release()
     cv2.destroyAllWindows()
 
 def f2():
@@ -129,12 +82,8 @@ def f2():
             argx = (100,100)
             argy = (100,100)
             for i in range(1, len(pts)):
-                # if either of the tracked points are None, ignore
-                # them
                 if pts[i - 1] is None or pts[i] is None:
                     continue
-                # otherwise, compute the thickness of the line and
-                # draw the connecting lines
                 argy = argx
                 argx = pts[i]
 
@@ -155,6 +104,12 @@ def f2():
                     pyautogui.mouseDown(cur[0]+x_,cur[1]-y_,button='left')
 
 
+def f3(count):
+    if len(count) > 0 :
+        return True
+    else:
+        print("ERRORO")
+        return False
 
 #if __name__ == "__main__":
 i = input("start?")
